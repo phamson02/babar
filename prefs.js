8a37ec2c277289ec06790d72dc65eec5c651bcd0
phamson02 <phamtienson02@gmail.com>
2023-11-21 05:19:14 +0700

    Update preferences widget

--- prefs.js
+++ prefs.js
@@ -1,157 +1,130 @@
 // Preferences UI for BaBar GNOME Shell extension
 
 import Gio from 'gi://Gio';
-import Gtk from 'gi://Gtk';
+import Adw from 'gi://Adw';
 
-function init() {
-}
+import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
-function make_item(label, schema, type, min, max) {
-    this.item_label = new Gtk.Label({
-        label: label,
-        margin_start: 14,
-        halign: Gtk.Align.START,
-        visible: true
-    });
-    let grid = new Gtk.Grid({
-    	visible: true,
-    	margin_start: 18,
-        margin_end: 18,
-    	margin_top: 2,
-    	margin_bottom: 2,
-    	column_spacing: 96
-    });
-    grid.attach(this.item_label, 0, 0, 1, 1);
+export default class MyPrefsWidget extends ExtensionPreferences {
+    fillPreferencesWindow(window) {
+		this.window = window;
+        this._initSettings();
+    }
 
-	if (type == 'b') {
-		this.item_value = new Gtk.Switch({
-		    active: this.settings.get_boolean(schema),
-		    halign: Gtk.Align.END,
-		    hexpand: true,
-		    visible: true
-		});
+    // Initialize settings
+    _initSettings() {
+		this._settings = this.getSettings("org.gnome.shell.extensions.babar");
+
+        const page = new Adw.PreferencesPage({
+            title: _('General'),
+            icon_name: 'dialog-information-symbolic',
+        });
+		this.page = page;
+        this.window.add(this.page);
+
+		// items
+		this.group = this.make_section_title('Elements (default value)');
+		this.page.add(this.group);
+
+		this.make_item('Activities (false)', 'display-activities', 'b');
+		this.make_item('Applications grid (true)', 'display-app-grid', 'b');
+		this.make_item('Favorites menu (true)', 'display-favorites', 'b');
+		this.make_item('Workspaces (true)', 'display-workspaces', 'b');
+		this.make_item('Tasks (true)', 'display-tasks', 'b');
+		this.make_item('Application menu (false)', 'display-app-menu', 'b');
+		this.make_item('Dash in overview (true)', 'display-dash', 'b');
+		this.make_item('Workspaces thumbnails in overview (true)', 'display-workspaces-thumbnails', 'b');
 		
-		grid.attach(this.item_value, 1, 0, 1, 1);
-
-    	this.settings.bind(
-		    schema,
-		    this.item_value,
-		    'active',
-		    Gio.SettingsBindFlags.DEFAULT
-    	);
-	}
-	
-	if (type == 'i') {
-		this.item_adjustment = new Gtk.Adjustment({
-			lower: min,
-			upper: max,
-			step_increment: 1
-		});
-		this.item_value = new Gtk.SpinButton({
-			adjustment: this.item_adjustment,
-			value: this.settings.get_int(schema),
-		    halign: Gtk.Align.END,
-		    hexpand: true,
-		    visible: true
-		});
+		this.group = this.make_section_title('Appearance (default value)');
+		this.page.add(this.group);
 		
-		grid.attach(this.item_value, 1, 0, 1, 1);
-
-		this.settings.bind(
-		    schema,
-		    this.item_value,
-		    'value',
-		    Gio.SettingsBindFlags.DEFAULT
-		);
-	}
-	
-	if (type == 's') {
-		this.item_value = new Gtk.Entry({
-			text: this.settings.get_string(schema),
-		    halign: Gtk.Align.END,
-		    hexpand: true,
-		    visible: true
-		});
+		this.make_item('Reduce elements padding (true)', 'reduce-padding', 'b');
+		this.make_item('Places extension label to icon (true)', 'display-places-icon', 'b');
+		this.make_item('Rounded workspaces icons (false)', 'rounded-workspaces-buttons', 'b');
+		this.make_item('Plain workspaces icons (false)', 'plain-workspaces-buttons', 'b');
+		this.make_item('Remove color from tasks icons (false)', 'desaturate-icons', 'b');
+		this.make_item('Move panel to the bottom of the screen (false)', 'bottom-panel', 'b');
+		this.make_item('Task icon size (18: Shell <= 3.38, 20: Shell >= 40)', 'icon-size', 'i', 8, 64);
+		this.make_item('Thumbnail maximum size % (25)', 'thumbnail-max-size', 'i', 10, 100);
+		// this.make_item('Applications grid icon (view-app-grid-symbolic)', 'app-grid-icon-name', 's');
+		// this.make_item('Places icon (folder-symbolic)', 'places-icon-name', 's');
+		// this.make_item('Favorites icon (starred-symbolic)', 'favorites-icon-name', 's');
 		
-		grid.attach(this.item_value, 1, 0, 1, 1);
+		this.group = this.make_section_title('Behavior (default value)');
+		this.page.add(this.group);
+		
+		this.make_item('Workspaces: left click to show, right-click to show or toggle overview (false)', 'workspaces-right-click', 'b');
+		this.make_item('Tasks: right-click to show window thumbnail (true)', 'right-click', 'b');
+		this.make_item('Tasks: middle-click to close window (true)', 'middle-click', 'b');
+		this.make_item('Tasks: sort favorites first (false)', 'favorites-first', 'b');
 
-		this.settings.bind(
-		    schema,
-		    this.item_value,
-		    'text',
-		    Gio.SettingsBindFlags.DEFAULT
-		);
+		this.window._settings = this._settings;
+    }
+
+	make_item(label, schema, type, min, max) {
+		// Get default value in parenthesis in label
+		let default_value = "Default value: " + label.match(/\(([^)]+)\)/)[1];
+		label = label.replace(/\([^)]+\)/, '');
+
+		if (type == 'b') {
+			this.item_value = new Adw.SwitchRow({
+				title: _(label),
+				subtitle: _(default_value),
+				active: this._settings.get_boolean(schema),
+			});
+
+			this._settings.bind(
+				schema,
+				this.item_value,
+				'active',
+				Gio.SettingsBindFlags.DEFAULT
+			);
+		}
+		
+		if (type == 'i') {
+			this.item_adjustment = new Gtk.Adjustment({
+				lower: min,
+				upper: max,
+				step_increment: 1
+			});
+
+			this.item_value = new Adw.SpinRow({
+				title: _(label),
+				subtitle: _(default_value),
+				adjustment: this.item_adjustment,
+				value: this._settings.get_int(schema),
+			});
+
+			this._settings.bind(
+				schema,
+				this.item_value,
+				'value',
+				Gio.SettingsBindFlags.DEFAULT
+			);
+		}
+		
+		if (type == 's') {
+			this.item_value = new Adw.EntryRow({
+				title: _(label),
+				subtitle: _(default_value),
+				text: this._settings.get_string(schema),
+			});
+
+			this._settings.bind(
+				schema,
+				this.item_value,
+				'text',
+				Gio.SettingsBindFlags.DEFAULT
+			);
+		}
+		this.group.add(this.item_value);
 	}
-    this.list['append'](grid);
-}
 
-function make_section_title(title) {
-	this.section_title = new Gtk.Label({
-        label: '<b>' + title + '</b>',
-        halign: Gtk.Align.START,
-        use_markup: true,
-        margin_start: 8,
-        margin_top: 2,
-    	margin_bottom: 2,
-        visible: true,
-    });
-    this.list['append'](this.section_title);
-    
-}
+	make_section_title(title) {
+		const group = new Adw.PreferencesGroup({
+            title: _(title),
+        });
 
-function buildPrefsWidget() {
-	this.settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.babar');
-
-	this.prefsWidget = new Gtk.ScrolledWindow({
-		visible: true,
-		margin_start: 18,
-        margin_end: 18,
-        margin_top: 18,
-        margin_bottom: 18,
-        vexpand: true,
-		hscrollbar_policy: Gtk.PolicyType.NEVER,
-		vscrollbar_policy: Gtk.PolicyType.AUTOMATIC
-	});
-    this.list = new Gtk.ListBox({
-        selection_mode: null,
-        can_focus: false,
-        visible: true
-    });
-    this.prefsWidget['set_child'](this.list);
-
-	// items
-    make_section_title('Elements (default value)');
-
-	make_item('Activities (false)', 'display-activities', 'b');
-    make_item('Applications grid (true)', 'display-app-grid', 'b');
-    make_item('Favorites menu (true)', 'display-favorites', 'b');
-    make_item('Workspaces (true)', 'display-workspaces', 'b');
-    make_item('Tasks (true)', 'display-tasks', 'b');
-    make_item('Application menu (false)', 'display-app-menu', 'b');
-    make_item('Dash in overview (true)', 'display-dash', 'b');
-    make_item('Workspaces thumbnails in overview (true)', 'display-workspaces-thumbnails', 'b');
-    
-    make_section_title('Appearance (default value)');
-    
-    make_item('Reduce elements padding (true)', 'reduce-padding', 'b');
-    make_item('Places extension label to icon (true)', 'display-places-icon', 'b');
-    make_item('Rounded workspaces icons (false)', 'rounded-workspaces-buttons', 'b');
-    make_item('Plain workspaces icons (false)', 'plain-workspaces-buttons', 'b');
-    make_item('Remove color from tasks icons (false)', 'desaturate-icons', 'b');
-    make_item('Move panel to the bottom of the screen (false)', 'bottom-panel', 'b');
-    make_item('Task icon size (18: Shell <= 3.38, 20: Shell >= 40)', 'icon-size', 'i', 8, 64);
-	make_item('Thumbnail maximum size % (25)', 'thumbnail-max-size', 'i', 10, 100);
-    make_item('Applications grid icon (view-app-grid-symbolic)', 'app-grid-icon-name', 's');
-    make_item('Places icon (folder-symbolic)', 'places-icon-name', 's');
-    make_item('Favorites icon (starred-symbolic)', 'favorites-icon-name', 's');
-    
-    make_section_title('Behavior (default value)');
-    
-	make_item('Workspaces: left click to show, right-click to show or toggle overview (false)', 'workspaces-right-click', 'b');
-    make_item('Tasks: right-click to show window thumbnail (true)', 'right-click', 'b');
-    make_item('Tasks: middle-click to close window (true)', 'middle-click', 'b');
-	make_item('Tasks: sort favorites first (false)', 'favorites-first', 'b');
-	
-    // return widget
-    return this.prefsWidget;
+		return group;
+	}
 }
